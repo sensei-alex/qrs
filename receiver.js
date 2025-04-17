3eea0a0db09a6314c3b978a5035d1f8f20bdc379
Alex <alexanderthestudent@yandex.ru>
2025-04-17 10:02:24 +0300

    Revert "fix: restore the variable"
    This reverts commit afdcc07797ea8e799e45cc9e52369815728b5592.

--- receiver.js
+++ receiver.js
@@ -51,16 +51,12 @@ function processPacket(data) {
     display.innerHTML = "";
     display.appendChild(area);
   } else if (data.type === "file") {
-    const url = data.file;
-
     const link = document.createElement("a");
+    link.href = data.file;
     link.download = data.name;
-    link.href = url;
-
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-    URL.revokeObjectURL(url);
   }
 }
 
