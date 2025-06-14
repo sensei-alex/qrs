14d9f8fe113bf899844a23c68c2c007925ed7e69
Alex <alexanderthestudent@yandex.ru>
2025-06-14 23:00:33 +0300

    fix: onConnect

--- lib.js
+++ lib.js
@@ -3,7 +3,7 @@
  * @async
  * @param {Object} params - The config
  * @param {string} params.id - This node's UUID. Generate a random one if you don't have it.
- * @param {Function} params.onConnect - When another node connects to this one
+ * @param {Function} params.onConnect - When another node connects to this one, passes the `sendData` function as the argument
  * @param {Function} params.onDisconnect - When another node disconnects
  * @param {Function} params.onData - When another node sends something
  * @returns {Promise<{ sendData: (data: Record<string, any>) => Promise<void>, connect: (peerId: string) => Promise<void> }>} Resolves when the coordination server has accepted the connection
@@ -13,7 +13,7 @@ async function setupNode({id, onConnect, onDisconnect, onData}) {
     const node = new Peer(id, {
       host: "peer-server.snlx.net",
       port: 443,
-      debug: 3,
+      // debug: 3,
       path: "/",
       config: {
         iceServers: [
@@ -25,25 +25,29 @@ async function setupNode({id, onConnect, onDisconnect, onData}) {
 
     console.log('creating a node')
 
+    async function connect(peerId) {
+      console.log('connecting')
+      const connection = await node.connect(peerId);
+
+      openConnections.push(connection)
+    }
+
+    async function sendData(packet) {
+      console.log('sending a packet')
+      await Promise.allSettled(openConnections.map(conn => conn.send(packet)))
+    }
+
     const openConnections = [];
 
     node.on("open", () => resolve({
-      async connect(peerId) {
-        console.log('connecting')
-        const connection = await node.connect(peerId);
-
-        openConnections.push(connection)
-      },
-      async sendData(packet) {
-        console.log('sending a packet')
-        await Promise.allSettled(openConnections.map(conn => conn.send(packet)))
-      },
+      connect,
+      sendData
     }))
 
     node.on("connection", (connection) => {
       console.log('saving a new connection')
       openConnections.push(connection)
-      onConnect(connection)
+      onConnect(sendData)
       connection.on("data", onData)
       connection.on("close", onDisconnect)
     })
